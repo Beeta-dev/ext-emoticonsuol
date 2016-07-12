85914ba6e5bbd02fe23f226a5fcf370da40936a3
Marco Aurélio Vieira (blurkness) <blurkness@gmail.com>
2016-07-12 15:07:23 -0300

    Sup

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Beeta\EmoticonsUol;
+
+use Flarum\Event\ConfigureFormatter;
+use Illuminate\Events\Dispatcher;
+
+return function (Dispatcher $events)
+{
+	$events->listen(
+		ConfigureFormatter::class,
+		function (ConfigureFormatter $event)
+		{
+			$event->configurator->Emoticons->add(':omg3:', '<img src="http://www.blurkness.com/flarum/assets/emos/icon_omgrollt.gif" alt=":omg3:" title="OMG">');
+		}
+	);
+};
