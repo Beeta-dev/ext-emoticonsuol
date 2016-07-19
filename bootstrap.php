3093f0a8678a2bc07b52c1aaa06e115d235c0c41
blurkness <blurkness@gmail.com>
2016-07-19 14:41:12 -0300

    Adicionando os emoticons na ext

--- bootstrap.php
+++ bootstrap.php
@@ -8,10 +8,7 @@ function subscribe(Dispatcher $events)
 		ConfigureFormatter::class,
 		function (ConfigureFormatter $event)
 		{
-			$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-			$base_url = $protocol.$_SERVER['HTTP_HOST'];
-			$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-			$base_url = rtrim($base_url, "/");
+			$base_url = realpath(__DIR__ . '/../../assets/emos');
 			$event->configurator->Emoticons->add(':-D', '<img src="'.$base_url.'/assets/emos/icon_biggrin.gif" alt=":-D" title="OMG">');
 			$event->configurator->Emoticons->add(':-)', '<img src="'.$base_url.'/assets/emos/icon_smile.gif" alt=":omg3:" title="OMG">');
 			$event->configurator->Emoticons->add(':-(', '<img src="'.$base_url.'/assets/emos/icon_sad.gif" alt=":omg3:" title="OMG">');
